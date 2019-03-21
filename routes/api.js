7426d1c8ffb76a7e471493ae12312b02b53a4c61
LibLogic <hdgknsn@gmail.com>
2019-03-21 16:28:43 +0000

    some work on schema, added some pseudo code to main.js, added a time-in route and a time-out route to api.js

--- routes/api.js
+++ routes/api.js
@@ -2,35 +2,50 @@ const express = require("express")
 const router = express.Router();
 const Employee = require("../models/Employee");
 
-router.get('/employees', 
+router.get('/', 
 function(req, res, next){
   Employee.find({}).then(function(employee){
     res.send(employee);
   }).catch(next);
 });
 
-router.post('/employees', function(req, res, next){
+router.post('/', function(req, res, next){
   Employee.create(req.body).then(function(employee){
     res.send(employee);
   }).catch(next);
 });
 
-
-// {
-// 	"$currentDate": {"timeOut": true}
-// }
-router.put('/employees/:id', function(req, res, next){
-  Employee.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(employee){
+router.put('/:id', function(req, res, next){
+  Employee.findOneAndUpdate({_id: req.params.id}, req.body).then(function(employee){
      Employee.findOne({_id: req.params.id}).then(function(employee){
       res.send(employee);
     });
   }).catch(next);
 });
 
-router.delete('/employees/:id', function(req, res, next){
-  Employee.findByIdAndDelete({_id: req.params.id}).then(function(employee){
+router.delete('/:id', function(req, res, next){
+  Employee.findOneAndDelete({_id: req.params.id}).then(function(employee){
     res.send(employee);
   }).catch(next);
 });
+
+router.put('/intime/:id', function(req, res, next){
+  var timeIn = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+  Employee.findOneAndUpdate({_id: req.params.id}, {$push: { "timeIn": timeIn }}).then(function(employee){
+     Employee.findOne({_id: req.params.id}).then(function(employee){
+      res.send(employee);
+    });
+  }).catch(next);
+});
+
+router.put('/outtime/:id', function(req, res, next){
+  var timeOut = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+  Employee.findOneAndUpdate({_id: req.params.id}, {$push: { "timeOut": timeOut }}).then(function(employee){
+     Employee.findOne({_id: req.params.id}).then(function(employee){
+      res.send(employee);
+    });
+  }).catch(next);
+});
+
 
 module.exports = router;
