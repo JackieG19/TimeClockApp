65d7fb9d1fc8d1c4c6e4bdacf433bb5da4da9774
Jackie <jacgut19@gmail.com>
2019-03-23 23:52:10 +0000

    working on linking the express routes to the html page

--- models/Employee.js
+++ models/Employee.js
@@ -1,15 +1,40 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const SessionSchema = new Schema({
+    session: {Array}
+});
+
 const EmployeeSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    employeeId: Number,
-    department: String,
-    timeIn: String,
-    timeOut: String,
-    companyName: String,
-    clockedIn: Boolean
+    firstName: {
+        type: String
+    },
+    
+    lastName: {
+        type: String
+    },
+    
+    employeeId:{
+        type: Number,
+        required: true
+    },
+    companyName: {
+        type: String
+    },
+    
+    deptName: {
+        type: String
+    },
+    
+    timeIn:Array,
+    
+    timeOut: Array,
+    
+    sessionTotals: {SessionSchema},
+    
+    isClockedIn: {
+        type: Boolean
+    }
 });
 
 const Employees = mongoose.model('employee', EmployeeSchema);
--- routes/api.js
+++ routes/api.js
@@ -3,21 +3,25 @@ const router = express.Router();
 const Employees = require('../models/Employee.js');
 
 // get a list of employee from the database
-router.get('/employee', function(req, res, next){
+router.get('/', function(req, res, next){
   Employees.find({}).then(function(employees){
     res.send(employees);
   }).catch(next);
 });
 
+// To select data from a table in MongoDB, we can also use the find() method.
+// The find() method returns all occurrences in the selection.
+
 // add to the db
-router.post('/employee', function(req, res, next){
+router.post('/', function(req, res, next){
     Employees.create(req.body).then(function(employees){
-        res.send(employees);
+        res.send(employees); // this method is used to send the HTTP response
     }).catch(next);
 });
 
+
 // update the db
-router.put('/employee/:id', function(req, res, next){
+router.put('/:id', function(req, res, next){
     Employees.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
         Employees.findOne({_id: req.params.id}).then(function(employees){
             res.send(employees);
@@ -25,11 +29,37 @@ router.put('/employee/:id', function(req, res, next){
     }).catch(next);
 });
 
+// The findOne() method returns the first occurrence in the selection.
+
 // delete from the db
-router.delete('/employee/:id', function(req, res, next){
+router.delete('/:id', function(req, res, next){
     Employees.findByIdAndRemove({_id: req.params.id}).then(function(employees){
         res.send(employees);
     }).catch(next);
 });
+
+// grabbing the employee clocking in
+router.put('/intime/:id', function(req, res, next){
+  var timeIn = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+  Employees.findOneAndUpdate({_id: req.params.id}, {$push: { "timeIn": timeIn }}).then(function(employee){
+     Employees.findOne({_id: req.params.id}).then(function(employee){
+      res.send(employee);
+    });
+  }).catch(next);
+});
+
+
+// grabbing the employee clocking out
+router.put('/outtime/:id', function(req, res, next){
+  var timeOut = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
+  Employees.findOneAndUpdate({_id: req.params.id}, {$push: { "timeOut": timeOut }}).then(function(employee){
+     Employees.findOne({_id: req.params.id}).then(function(employee){
+      res.send(employee);
+    });
+  }).catch(next);
+});
+
+// new Date() creates a new date object with the current date and time:
+// The toLocaleString() method returns a string with a language sensitive representation of this date.
 
 module.exports = router;
